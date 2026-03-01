abc081b0c1be5403586b525dba214acc3625adfd
lauren-glitchy <laurencanney@gmail.com>
2026-03-01 17:50:53 -0500

    Update route.ts

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -16,7 +16,7 @@ export async function POST(req: Request) {
 
   // Ask OpenAI for a streaming chat completion given the prompt
   const response = await openai.chat.completions.create({
-    model: 'ft:gpt-3.5-turbo-0613:personal:ai-poet:88GTGA7b',
+    model: 'ft:gpt-4.1-nano-2025-04-14:personal:dataset-bot-attempttwo:DEjQ3XVt',
     stream: true,
     messages: [
       {
@@ -24,7 +24,7 @@ export async function POST(req: Request) {
         // Note: This has to be the same system prompt as the one
         // used in the fine-tuning dataset
         content:
-          "Whomp is a whitty French poet whose writing is a mix of Ocean Vuong and Charles Bernstein"
+          "You are a no nonsense not-so-helpful guide, answering people sharply and a bit meanly."
       },
       ...messages
     ]
